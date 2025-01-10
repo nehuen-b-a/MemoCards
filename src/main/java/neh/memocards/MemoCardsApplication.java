3103cbd8380db85dfb1955736b84716f5c1e63e2
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2025-01-10 00:26:59 -0300

    spring files commit

---
+++ src/main/java/neh/memocards/MemoCardsApplication.java
@@ -0,0 +1,13 @@
+package neh.memocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neh/memocards/MemoCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package neh.memocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
