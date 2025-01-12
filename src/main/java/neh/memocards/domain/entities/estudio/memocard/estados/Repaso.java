5a4ff05e3546eb65eca970da9fe24a671ad357ae
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2025-01-12 16:31:07 -0300

    src: implemento el Calculo de Intervalos de Repaso

--- src/main/java/neh/memocards/domain/entities/estudio/memocard/estados/Repaso.java
+++ src/main/java/neh/memocards/domain/entities/estudio/memocard/estados/Repaso.java
@@ -5,28 +5,29 @@ import neh.memocards.domain.entities.estudio.memocard.MemoCard;
 
 public class Repaso extends EstadoMemoCard {
     // Atributos
-    private Double coeficienteDeRetencion;
-    private Double coeficienteDeBonusPorFacilidad;
-    private Double coeficientePorDificultad;
-    private Long intervaloMax;
+    private final Double coeficienteDeRetencion;
+    private final Long intervaloMax;
 
     // Métodos
     @Override
-    public Long calcularIntervalo(Long intervalo, Integer dificultad) {
-
-        return null;
+    public Long calcularIntervalo(Long intervaloAnterior, Integer dificultad) {
+        if(dificultad >= 3) {
+            this.actualizarEstado();
+            return super.getMemoCard().getEstadoAprendizaje().calcularIntervalo(intervaloAnterior, dificultad);
+        }
+        return (double)(intervaloAnterior) * coeficienteDeRetencion < this.intervaloMax?
+                (long)((double)(intervaloAnterior) * coeficienteDeRetencion)
+                : this.intervaloMax;
     }
 
     @Override
     public void actualizarEstado() {
-        // Implementación pendiente
+        super.getMemoCard().cambiarEstado(new Reaprendizaje(super.getMemoCard()));
     }
 
     public Repaso(MemoCard memoCard) {
         super(memoCard, "APRENDIZAJE");
         this.coeficienteDeRetencion = memoCard.getConfigurador().getFactorFacilidad();
-        this.coeficienteDeBonusPorFacilidad = memoCard.getConfigurador().getFactorFacilidad();
-        this.coeficientePorDificultad = memoCard.getConfigurador().getIntervaloDificil();
         this.intervaloMax = memoCard.getConfigurador().getIntervaloMaximo();
     }
 }
