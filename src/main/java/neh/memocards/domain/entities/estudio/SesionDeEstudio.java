a5c92c908ed860eccf4d1645bb2ac8441fc14053
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2025-01-17 12:39:52 -0300

    src: implemento SesionDeEstudio

--- src/main/java/neh/memocards/domain/entities/estudio/SesionDeEstudio.java
+++ src/main/java/neh/memocards/domain/entities/estudio/SesionDeEstudio.java
@@ -1,37 +1,117 @@
 package neh.memocards.domain.entities.estudio;
 
+import lombok.Getter;
+import lombok.Setter;
+import neh.memocards.domain.entities.estudio.memocard.Dificultad;
 import neh.memocards.domain.entities.estudio.memocard.MemoCard;
 
 import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
+@Getter
 public class SesionDeEstudio {
     // Atributos
-    private Long id;
-    private Estudiante estudiante;
-    private Mazo mazo;
-    private Long tiempoEstudio;
-    private Set<MemoCard> tarjetasRevisadas;
-    private LocalDateTime fechaInicio;
-    private LocalDateTime fechaFin;
-    private Boolean estaFinalizada;
+    @Setter
+    Long id;
+    @Setter
+    Estudiante estudiante;
+    @Setter
+    Mazo mazo;
+    @Setter
+    Long tiempoEstudio;
+    Set<MemoCard> memoCardsRevisadas;
+    @Setter
+    Integer cantidadTotalIntentos;
+    @Setter
+    Integer cantidadTotalAciertos;
+    @Setter
+    Integer cantidadTotalDesaciertos;
+    @Setter
+    Integer cantidadTotalDificil;
+    @Setter
+    Integer cantidadTotalBien;
+    @Setter
+    Integer cantidadTotalFacil;
+    LocalDateTime fechaInicio;
+    LocalDateTime fechaFin;
+    @Setter
+    Boolean estaFinalizada;
 
     // Métodos
-    public void iniciarSesion() {
-        // Implementación pendiente
+
+    public SesionDeEstudio() {
+        this.tiempoEstudio = 0L;
+        this.memoCardsRevisadas = new HashSet<>();
+        this.cantidadTotalIntentos = 0;
+        this.cantidadTotalAciertos = 0;
+        this.cantidadTotalDesaciertos = 0;
+        this.cantidadTotalDificil = 0;
+        this.cantidadTotalBien = 0;
+        this.cantidadTotalFacil = 0;
+        this.fechaInicio = null;
+        this.fechaFin = null;
+        this.estaFinalizada = false;
+    }
+    public SesionDeEstudio(List<MemoCard> memoCardsArevisar,Estudiante estudiante, Mazo mazo) {
+        new SesionDeEstudio();
+        this.estudiante = estudiante;
+        this.mazo = mazo;
+    }
+
+    public void comenzarSesion() {
+        this.fechaInicio = LocalDateTime.now();
+        memoCardsRevisadas.forEach(memo -> memo.setSesionDeEstudioActual(this));
     }
 
     public void finalizarSesion() {
-        // Implementación pendiente
+        this.fechaFin = LocalDateTime.now();
+        this.estaFinalizada = true;
+
+
+        if (this.fechaInicio != null && this.fechaFin != null) {
+            this.tiempoEstudio = java.time.Duration.between(this.fechaInicio, this.fechaFin).toSeconds();
+        }
+
+
+        memoCardsRevisadas.forEach(memo -> memo.setSesionDeEstudioActual(null));
     }
 
-    public void registrarMetrica() {
-        // Implementación pendiente
+    public void registrarMetrica(MemoCard memoCard, Dificultad dificultad) {
+
+        this.memoCardsRevisadas.add(memoCard);
+
+        switch (dificultad) {
+            case OLVIDO:
+                this.cantidadTotalDesaciertos++;
+                break;
+            case DIFICIL:
+                this.cantidadTotalDificil++;
+                this.cantidadTotalAciertos++;
+                break;
+            case BIEN:
+                this.cantidadTotalBien++;
+                this.cantidadTotalAciertos++;
+                break;
+            case FACIL:
+                this.cantidadTotalFacil++;
+                this.cantidadTotalAciertos++;
+                break;
+            default:
+                throw new IllegalArgumentException("Dificultad no válida: " + dificultad);
+        }
     }
 
     public Map<String, Object> obtenerMetrica() {
-        // Implementación pendiente
-        return null;
+        Map<String, Object> metricas = new HashMap<>();
+        metricas.put("cantidadTotalIntentos", this.cantidadTotalIntentos);
+        metricas.put("cantidadTotalAciertos", this.cantidadTotalAciertos);
+        metricas.put("cantidadTotalDesaciertos", this.cantidadTotalDesaciertos);
+        metricas.put("cantidadTotalDificil", this.cantidadTotalDificil);
+        metricas.put("cantidadTotalBien", this.cantidadTotalBien);
+        metricas.put("cantidadTotalFacil", this.cantidadTotalFacil);
+        metricas.put("tiempoEstudio", this.tiempoEstudio);
+        metricas.put("cantidadMemoCardsRevisadas", this.memoCardsRevisadas.size());
+        return metricas;
     }
+
 }
