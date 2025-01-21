ab53f9f59022c2fb4b3f98a9cd39d2217d886656
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2025-01-21 01:15:57 -0300

    src: itero el barajador de IntervaloMenorAMayor para implementar los nuevos metodos

--- src/main/java/neh/memocards/domain/entities/estudio/memocard/barajador/IntervaloMenorAMayor.java
+++ src/main/java/neh/memocards/domain/entities/estudio/memocard/barajador/IntervaloMenorAMayor.java
@@ -8,31 +8,43 @@ import utils.CircularList;
 import java.util.*;
 
 @NoArgsConstructor
-public class IntervaloMenorAMayor implements ITipoDeBarajador {
+public class IntervaloMenorAMayor implements ITipoBarajador {
 
     @Override
-    public CircularList<MemoCard> barajarComienzoDeSesion(List<MemoCard> memoCardsNuevas, List<MemoCard> memoCardsRepaso) {
+    public CircularList<MemoCard> barajarComienzoDeSesion(List<MemoCard> memoCardsNuevas, List<MemoCard> memoCardsRepaso, CircularList<MemoCard> memoCardsActivas ) {
         CircularList<MemoCard> memoCardsBarajadas = new CircularList<>();
         memoCardsBarajadas.addAll(memoCardsNuevas);
         memoCardsBarajadas.addAll(memoCardsRepaso);
 
-        return barajar(memoCardsBarajadas);
+        //Barajo
+        memoCardsBarajadas = barajar(memoCardsBarajadas);
+        memoCardsActivas = barajar(memoCardsActivas);
+
+        //añado Todas las memoCards ya barajadas a la listaActiva
+        memoCardsActivas.addAll(memoCardsBarajadas.getElements());
+
+        return memoCardsActivas;
     }
 
     @Override
     public CircularList<MemoCard> barajar(CircularList<MemoCard> memoCards) {
-        //Desordeno antes de Ordenar, para evitar patornes repetidos en el orden de las tarjetas
+        //Desordeno antes de Ordenar, esto evita patrones repetidos en el orden de las tarjetas
         List<MemoCard> memoCardsPorBarajadar = memoCards.getElements();
         Collections.shuffle(memoCardsPorBarajadar);
 
         //Ordeno
-        List<MemoCard> repaso = memoCardsPorBarajadar.stream().sorted(Comparator.comparingInt(MemoCard::getIntentos)).toList();
+        List<MemoCard> repaso = ordenarMemoCards(memoCardsPorBarajadar);
 
         //Retorno el valor como CircularList
         CircularList<MemoCard> memoCardsBarajadas = new CircularList<>();
         memoCardsBarajadas.addAll(repaso);
 
         return memoCardsBarajadas;
+    }
+
+    @Override
+    public List<MemoCard> ordenarMemoCards(List<MemoCard> memoCards) {
+        return memoCards.stream().sorted(Comparator.comparingInt(MemoCard::getIntentos)).toList();
     }
 
 
